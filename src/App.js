4a2373c5bd5c4cadce4735314fad7fdb82c57795
Yasitha Herath <naveendray@gmail.com>
2023-08-14 18:59:24 +0530

    Add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = "https://www.google.com"
+
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
