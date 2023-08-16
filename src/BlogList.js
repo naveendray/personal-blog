304bdf3dcd59e906aab79001626b137e308661a8
Yasitha Herath <naveendray@gmail.com>
2023-08-16 16:24:08 +0530

    modify program to take data from json server

--- src/BlogList.js
+++ src/BlogList.js
@@ -10,7 +10,6 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by {blog.author}</p>
-                    <button onClick={() => props.handleDelete(blog.id)}>Delete blog</button>
                 </div>
             ))}
         </div>
