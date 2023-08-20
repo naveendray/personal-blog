63dddcd5a52073875a5429fafd10d172006ffc12
Yasitha Herath <naveendray@gmail.com>
2023-08-20 17:19:38 +0530

    clean fetch url

--- src/Home.js
+++ src/Home.js
@@ -6,9 +6,7 @@ const Home = () => {
     const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        fetch("http://127.0.0.1:49812/blogs")
-        // fetch("http://json-service:8000/blogs")
-        // fetch("http://localhost:8000/blogs")
+        fetch("http://localhost:8000/blogs")
         .then(res => {
          return res.json()
       }).then(data => {
