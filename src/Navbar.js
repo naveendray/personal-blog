509ae3ef629699731bb55bcff7735adf52d5aef1
Yasitha Herath <naveendray@gmail.com>
2023-08-29 18:51:41 +0530

    stage-23-replace href with Link

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>My Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
-                <a href="/test">Test Rout</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
+                <Link to="/test">Test Rout</Link>
             </div>
         </nav>
      );
