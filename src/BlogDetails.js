be41bca87b07be22c4434e338debf4daf5cf25df
Yasitha Herath <naveendray@gmail.com>
2023-08-29 22:39:02 +0530

    stage-26-add blog details functions

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom";
+import useFetch from "./useFetch";
 
-const BlogDetails = () => {
-    const { id } = useParams()
+const BlogDetails = (props) => {
+    const { id } = useParams();
+    const { data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/'+id);
 
     return ( 
-        <div className="blog-details">
-            <h2>Blog Details - { id }</h2>
+        <div className="blog-details" >
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>Written by : {blog.author}</h2>
+                    <p>{ blog.body }</p>
+                </article>
+            )
+            }
         </div>
      );
 }
