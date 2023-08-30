73c21bcfeba2499d6576ec67ee64d9ea30c75bfc
Yasitha Herath <naveendray@gmail.com>
2023-08-30 21:25:41 +0530

    stage-30- redirect after creating a blog

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {useHistory} from 'react-router-dom';
 
 const Create = () => {
 
@@ -6,6 +7,7 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('');
     const [isLoading, setIsLoading] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,6 +20,8 @@ const Create = () => {
         }).then(() => {
             console.log("new blog added");
             setIsLoading(false);
+            // history.go(-1);
+            history.push('/')
         })
     }
 
