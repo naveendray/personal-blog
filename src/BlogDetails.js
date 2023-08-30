6495dda48ec212175912b825782141ca87e66fbf
Yasitha Herath <naveendray@gmail.com>
2023-08-30 22:50:22 +0530

    stage 31 add delete function to blogs

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,20 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = (props) => {
     const { id } = useParams();
     const { data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/'+id);
+    const history = useHistory();
+
+    const handleClick = () => {
+        console.log('clicked')
+        fetch('http://localhost:8000/blogs/'+blog.id,{
+            method: 'DELETE'
+        }).then(() => {
+            console.log('deleted')
+            history.push('/')
+        })
+    }
 
     return ( 
         <div className="blog-details" >
@@ -13,6 +24,7 @@ const BlogDetails = (props) => {
                 <article>
                     <h2>Written by : {blog.author}</h2>
                     <p>{ blog.body }</p>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )
             }
