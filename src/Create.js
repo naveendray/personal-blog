451e56189fd748b7647e91b46912d2c7e241c5ed
Yasitha Herath <naveendray@gmail.com>
2023-08-30 10:52:57 +0530

    stage-29- complete post request to json server

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,54 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+        const blog = {title,body,author}
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log("new blog added");
+            setIsLoading(false);
+        })
+    }
+
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label >Blog title: </label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label >Blog body: </label>
+                <textarea 
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author: </label>
+                <select 
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >   
+                    <option value="mario">mario</option>
+                    <option value="john">john</option>
+                </select >
+                {!isLoading && <button>Add blog</button>}
+                {isLoading && <button>Adding blog</button>}
+            </form>
         </div>
      );
 }
